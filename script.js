3b5dd14b7407f82266a1e9a0a1c0a20d571aebd8
NGAM TARYLL <taryllngam@gmail.com>
2023-04-18 16:25:49 +0100

    added functionality to tip calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+// const input1 = document.getElementById('input1')
+// const input2 = document.getElementById('input2')
+const btn = document.getElementById('btn')
+
+
+
+
+btn.addEventListener('click', finalTip)
+
+
+function tipAmount(){
+    const input1 = parseInt(document.getElementById('input1').value)
+    const input2 = parseInt(document.getElementById('input2').value)
+    return input1 * input2/100
+}
+
+function finalTip(){
+    document.getElementById('display').innerHTML = tipAmount()
+}
