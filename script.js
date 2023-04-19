e926a202186214ba16ec84b3fdfa1c452d8ade37
NGAM TARYLL <taryllngam@gmail.com>
2023-04-19 11:30:13 +0100

    linters check

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 // const input1 = document.getElementById('input1')
 // const input2 = document.getElementById('input2')
-const btn = document.getElementById('btn')
+const btn = document.getElementById("btn");
 
+btn.addEventListener("click", finalTip);
 
-
-
-btn.addEventListener('click', finalTip)
-
-
-function tipAmount(){
-    const input1 = parseInt(document.getElementById('input1').value)
-    const input2 = parseInt(document.getElementById('input2').value)
-    return input1 * input2/100
+function tipAmount() {
+    const input1 = parseInt(document.getElementById("input1").value);
+    const input2 = parseInt(document.getElementById("input2").value);
+    return (input1 * input2) / 100;
 }
 
-function finalTip(){
-    document.getElementById('display').innerHTML = tipAmount()
+function finalTip() {
+    document.getElementById("display").innerHTML = tipAmount();
 }
